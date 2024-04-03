7a9627efa92a747359067499fc99dcc8c9c93aa9
SauleBis <saule.bisengalieva@gmail.com>
2024-04-03 18:04:23 -0400

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.9-slim
+WORKDIR /app
+COPY . /app
+RUN pip3 install -r requirements.txt
+EXPOSE 8501
+ENTRYPOINT ["streamlit", "run", "streamlit_tips.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- streamlit_tips.py
+++ streamlit_tips.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 path = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv'
